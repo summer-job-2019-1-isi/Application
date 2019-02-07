e37070faf824aee4ef916b2f155e1c4670d690cd
KevinKAMS <kams@cin.ufpe.br>
2019-02-07 11:30:20 -0300

    Adição da ferramenta de mapa de calor colorido 1.0

--- src/main.py
+++ src/main.py
@@ -10,19 +10,22 @@ import initialInput, areaModule, tempModule, elecModule, math, operator, printLi
 
 class myProtocol:
     
-    def __init__(self, nome, alcance, snr, freq, rank):
+    def __init__(self, nome, alcance, snr, freq, temp, rank, cable):
         self.nome = nome
         self.alcance = alcance
         self.snr = snr
         self.freq = freq
+        self.temp = temp
         self.rank = rank
+        self.cable = cable
 
-wifi2 = myProtocol("Wifi 2.4Ghz", 400, 4, 2400, 0)
-wifi5 = myProtocol("Wifi 5Ghz", 200, 6, 5000, 0)
-lora = myProtocol("LoRa", 5000, 15, 915, 0)
-zigbee = myProtocol("ZigBee", 225, 9, 2400, 0)
-bluetooth = myProtocol("Bluetooth", 40, 4, 2400, 0)
-PLC = myProtocol("PLC", 100, 3, 30, 0)
+wifi2 = myProtocol("Wifi 2.4Ghz", 400, 4, 2400, 0, 0, False)
+wifi5 = myProtocol("Wifi 5Ghz", 200, 6, 5000, 0, 0, False)
+lora = myProtocol("LoRa", 5000, 15, 915, 0, 0, False)
+zigbee = myProtocol("ZigBee", 225, 9, 2400, 0, 0, False)
+bluetooth = myProtocol("Bluetooth", 40, 4, 2400, 0, 0, False)
+PLC = myProtocol("PLC", 100, 3, 30, 0, 0, False)
+
 
 
 
@@ -46,11 +49,11 @@ def recconFreq(freq):
     else:
         return -1
         
-def ituModel(freq, dist):
-    perda = (20*math.log10(freq)) - 28 + 30*math.log10(dist)
-    return perda
+#def ituModel(freq, dist):
+#    perda = (20*math.log10(freq)) - 28 + 30*math.log10(dist)
+#    return perda
 
-def rankProtocol(protocol, frequencias, nBlocos):
+def rankMiddle(protocol, frequencias, nBlocos):
     pontuacao = 0
     if(protocol.alcance < 10):
         pontuacao += nBlocos
@@ -60,16 +63,20 @@ def rankProtocol(protocol, frequencias, nBlocos):
         pontuacao +=3*nBlocos
     else:
         pontuacao +=4*nBlocos
+
+    if(protocol.cable == False):
+        if(protocol.snr < 3):
+            pontuacao += 1*nBlocos
+        elif(protocol.snr < 6):
+            pontuacao += 2*nBlocos
+        elif(protocol.snr < 12):
+            pontuacao += 3*nBlocos
+        else:
+            pontuacao += 4*nBlocos
     
-    if(protocol.snr < 3):
-        pontuacao += 1*nBlocos
-    elif(protocol.snr < 6):
-        pontuacao += 2*nBlocos
-    elif(protocol.snr < 12):
-        pontuacao += 3*nBlocos
-    else:
-        pontuacao += 4*nBlocos
-    
+    #if(protocol.cable == True)
+
+
     for x in range(0, nBlocos):
         if (int(frequencias[x][recconFreq(protocol.freq)]) >= 50 + protocol.snr):
             pontuacao += 0
@@ -80,28 +87,6 @@ def rankProtocol(protocol, frequencias, nBlocos):
         else:
             pontuacao += 3
             
-    return pontuacao
-
-def pontuacaoBase(alcance, snr):
-    pontuacao = 0
-    if(alcance < 10):
-        pontuacao += 1
-    elif(alcance < 100):
-        pontuacao += 2
-    elif(alcance < 400):
-        pontuacao +=3
-    else:
-        pontuacao +=4
-    
-    if(snr < 3):
-        pontuacao += 1
-    elif(snr < 6):
-        pontuacao += 2
-    elif(snr < 12):
-        pontuacao += 3
-    else:
-        pontuacao += 4
-
     return pontuacao
 
 def rankBlock(pontuacaoBs, freq, frequencia):
@@ -133,6 +118,11 @@ def temperatureMap(temperatures, nBlocos):
     
     return temperatura
 
+def print50(value):
+    for x in range(0, 50):
+        file.write(str(value) + "\n")
+    return
+
 
 def main():
     #Dados iniciais do projeto
@@ -158,27 +148,32 @@ def main():
     #for x in range(0, nBlocos):
     #    print(" " + str(x+1) + "         " + str(frequencias[x][0]) + "      " + str(frequencias[x][1]) + "      " + str(frequencias[x][2]) + "     " + str(frequencias[x][3]) + "     " + str(frequencias[x][4]) + "     " + str(frequencias[x][5]) + "    " + str(frequencias[x][6]) + "    " + str(frequencias[x][7]))
 
-    wifi2.rank = rankProtocol(wifi2, frequencias, nBlocos)
-    lora.rank = rankProtocol(lora, frequencias, nBlocos)
-    zigbee.rank = rankProtocol(zigbee, frequencias, nBlocos)
-    PLC.rank = rankProtocol(PLC, frequencias, nBlocos)
+    wifi2.rank = rankMiddle(wifi2, frequencias, nBlocos)
+    lora.rank = rankMiddle(lora, frequencias, nBlocos)
+    zigbee.rank = rankMiddle(zigbee, frequencias, nBlocos)
+    PLC.rank = rankMiddle(PLC, frequencias, nBlocos)
 
     print("\n")
-    protocoloEscolhido = []
-    protocoloEscolhido.append(wifi2)
-    protocoloEscolhido.append(zigbee)
-    protocoloEscolhido.append(lora)
-    protocoloEscolhido.append(PLC)
-    protocoloEscolhido.sort(key=lambda x: x.rank, reverse = True)
-    #print(protocoloEscolhido)
-    for x in range(0, len(protocoloEscolhido)):
-        print(str(x+1) + " - " + protocoloEscolhido[x].nome)
+    nonCableMiddle = []
+    nonCableMiddle.append(wifi2)
+    nonCableMiddle.append(wifi5)
+    nonCableMiddle.append(zigbee)
+    nonCableMiddle.append(lora)
+    nonCableMiddle.append(PLC)
+    nonCableMiddle.sort(key=lambda x: x.rank, reverse = True)
+    #print(nonCableMiddle)
+    for x in range(0, len(nonCableMiddle)):
+        print(str(x+1) + " - " + nonCableMiddle[x].nome)
+
+    #cableMiddle = []
+
+    
 
     order = 1
     while(order != 0):
         order = int(input("Qual meio de comunicação deseja escolher? (Ou 0 para sair)\n"))
         if(order != 0):
-            mapaDeInterferencia = interferenceMap(frequencias, nBlocos, protocoloEscolhido[order-1].freq)
+            mapaDeInterferencia = interferenceMap(frequencias, nBlocos, nonCableMiddle[order-1].freq)
             mapaDeCalor = temperatureMap(temperatures, nBlocos)
             print("Mapa de interferencia:")
             printList.printList(mapaDeInterferencia)
@@ -186,7 +181,43 @@ def main():
             printList.printList(mapaDeCalor)
             print("\n")
 
+    
+
+
+    largura = int(math.sqrt(nBlocos))
+    if(int(math.ceil((nBlocos-(largura*largura))/largura)) > 0):
+        altura = int(math.ceil((nBlocos-(largura*largura))/largura))
+    else: 
+        altura = largura
+    nTotalDeBlocos = largura * altura
+    nBlocosCinzas = nTotalDeBlocos - nBlocos
+
+    vetorDeCor = []
+    vetorDeCor.extend(temperatures)
+    for x in range(0, nBlocosCinzas):
+        vetorDeCor.append(0)
+
+    print(vetorDeCor)
+
     file = open("InterferenceMap.ppm", "w")
+    file.write("P3\n")
+    file.write(str(50*largura) + " " + str(50*altura) + "\n")
+
+    print("largura é: "+ str(largura))
+        
+    cor = 0
+    for y in range(0, 50):
+        for x in range(0, (largura*50)):
+            if(x != 0 and x%50 == 0):
+                cor += 1
+                file.write(str(vetorDeCor[cor]) + "\n")
+            else:
+                file.write(str(vetorDeCor[cor]) + "\n")    
+        cor -= largura -1
+        
+    #file.write()
+    # largura sqrt = int(math.sqrt(nBlocos))
+    # altura sqrt + int((nBlocos%sqrt)) 
     
     #file = open("Results.txt","w")
     #file.write(nomeDoProjeto + "\n" + data + "\n")
